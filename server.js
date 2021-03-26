e216f9436b082c3d566e096b9986be2198e2c5af
vinaychauhan3012 <vinaychauhan3012@gmail.com>
2021-03-27 03:48:09 +0530

    Second Commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const axios = require('axios') // to make http request
 
 const app= express()
+const SERVER_PORT = process.env.PORT || 3366;
 let rollNo=[]
 
 app.use(express.urlencoded({extended : true}))
@@ -35,6 +36,6 @@ app.post('/',(req,res)=>{
 })
 
 
-app.listen(3366,()=>{
+app.listen(SERVER_PORT,()=>{
   console.log('Server started at localhost:3366')
 })
